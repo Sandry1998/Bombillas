338ff31806aa9a26ce3f976da0cdabdb5b3427ac
Sandry1998 <slopez@ceatformacion.com>
2025-07-07 16:37:17 +0200

    TRABAJOS

--- src/Bombilla.java
+++ src/Bombilla.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Bombilla {
+
+    static public boolean interruptorGeneral=true;
+    private boolean interruptor;
+
+    //constructor vacío, para crear el objeto bombilla en estado apagado
+    public Bombilla() {
+        this.interruptor=false;
+    }
+
+    //Método que enciende o apaga segun como este el interruptor
+    public void encenderOapagar(){
+        if(this.interruptor){
+            this.interruptor=false;
+        }else{
+            this.interruptor=true;
+        }
+    }
+    //Método para cambiar estado
+    public boolean estado() {
+        return interruptorGeneral && this.interruptor;
+    }
+
+    //Método para mostrar estado
+
+    public String mostrarEstado() {
+        return estado()?" Encendido " : " Apagado ";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        menu();
+
+    }
+
+    static void menu() {
+        Scanner sc = new Scanner(System.in);
+        Bombilla salon = new Bombilla();
+        Bombilla cocina = new Bombilla();
+        Bombilla hab1 = new Bombilla();
+        Bombilla hab2 = new Bombilla();
+        Bombilla patio = new Bombilla();
+        int opcion;
+        do {
+            System.out.println("Que bombilla deseo utilizar");
+            System.out.println("1. Salón " + "[" + salon.mostrarEstado() + "]");
+            System.out.println("2. Cocina " + "[" + cocina.mostrarEstado() + "]");
+            System.out.println("3. Habitación Principal " + "[" + hab1.mostrarEstado() + "]");
+            System.out.println("4. Habitación Niños " + "[" + hab2.mostrarEstado() + "]");
+            System.out.println("5. Patio " + "[" + patio.mostrarEstado() + "]");
+            System.out.println("6. Interruptor General " + "[" + Bombilla.interruptorGeneral + "]");
+            System.out.println("7. Salir ");
+            System.out.println("Indique su Opcion--> ");
+            opcion = sc.nextInt();
+            String mensaje = "El estado de la bombilla es: ";
+            switch (opcion) {
+                case 1 -> {
+                    salon.encenderOapagar();
+                }
+                case 2 -> {
+                    cocina.encenderOapagar();
+                }
+                case 3 -> {
+                    hab1.encenderOapagar();
+                }
+                case 4 -> {
+                    hab2.encenderOapagar();
+                }
+                case 5 -> {
+                    patio.encenderOapagar();
+                }
+                case 6 -> {
+                    if (Bombilla.interruptorGeneral) {
+                        Bombilla.interruptorGeneral = false;
+                    } else {
+                        Bombilla.interruptorGeneral = true;
+                    }
+                }
+                default -> System.out.println("Opcion no valida");
+
+            }
+
+        } while (opcion != 6);
     }
 }
